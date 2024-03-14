fea2c521920a4025448cc2279353e7c5e35ebb41
Jason <jdswart2@svsu.edu>
2024-03-13 21:50:15 -0400

    Read source code from file

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,43 @@
 #include "lexer.hh"
+#include <fstream>
+#include <sstream>
 
-int main () {
-  auto code = "fn 123\n\n main \"hi\" 3.14 (){} true false;";
-  auto stringTable = StringTable{};
-  auto lexer = Lexer(code, &stringTable);
-
-  try {
-    std::optional<Token> token;
-    while ((token = lexer.nextToken())) {
-      std::cout << *token << "\n";
-    }
-  } catch (UnclosedDelimiter d) {
-    std::cout << "Error: Unclosed delimeter beginning at line " << lexer.currentLine << "\n";
+std::optional<std::string> readFile(const std::string& filename) {
+  std::ifstream file(filename, std::ios::binary);
+  if (!file.is_open()) {
+    std::cerr << "Error: Could not open file " << filename << "\n";
+    return std::nullopt;
   }
 
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string res = buffer.str();
+
+  return res;
+}
+
+int main (int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Error: expected file path as argument\n";
+    return 1;
+  }
+
+  auto file = readFile(argv[1]);
+  if (file.has_value()) {
+    std::string code = file.value();
+    auto stringTable = StringTable{};
+    auto lexer = Lexer(code, &stringTable);
+
+    try {
+      std::optional<Token> token;
+      while ((token = lexer.nextToken())) {
+	std::cout << *token << "\n";
+      }
+    } catch (UnclosedDelimiter _) {
+      std::cerr << "Error: Unclosed delimeter beginning at line " << lexer.currentLine << "\n";
+      return 1;
+    }
+  }
+  
   return 0;
 }
